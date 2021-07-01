19dedf3423d4c59a3f1c42ba76d51246cb950760
Aditi Dwivedi <addoo96@gmail.com>
2021-07-01 16:40:25 +0100

    Initial Commit

---
+++ src/components/SliderData.js
@@ -0,0 +1,14 @@
+export const SliderData = [
+    {
+        image: 'https://images.unsplash.com/photo-1527251672045-a80241b3f574?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDN8Ym84alFLVGFFMFl8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1590695577457-48bd93c13f84?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDEyfGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1604252879330-a63fadf53b63?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE1fGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60'
+    },
+    {
+        image: 'https://images.unsplash.com/photo-1588426651288-7e828a77f413?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI1fGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60'
+    }
+]
